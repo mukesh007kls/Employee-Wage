593413f7a397ba2b032e5eb2c82373fcdaae79b8
mukesh007kls <mallikanti2000@gmail.com>
2023-02-23 06:19:55 +0530

    ADDED:Code To Find Employee Wage

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        int FULL_TIME = 1;
+        final int FULL_TIME = 1;
+        final int EMPLOYEE_WAGE_PER_HOUR=20;
+        int employeeHours=0;
+        int employeeWage=0;
         double empPresent = Math.floor(Math.random() * 10) % 2;
         if (empPresent == FULL_TIME) {
             System.out.println("Employee Present");
+            employeeHours=8;
         } else {
             System.out.println("Employee Absent");
+            employeeHours=0;
         }
+        employeeWage=employeeHours*EMPLOYEE_WAGE_PER_HOUR;
+        System.out.println("Employee Wage:"+employeeWage);
     }
 }
