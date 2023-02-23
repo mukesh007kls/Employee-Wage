3ac95c540b866a3baefefe7a83ae754686f0c11e
mukesh007kls <mallikanti2000@gmail.com>
2023-02-23 06:41:59 +0530

    ADDED:Code for Part Time

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         final int FULL_TIME = 1;
+        final int PART_TIME=2;
         final int EMPLOYEE_WAGE_PER_HOUR = 20;
         int employeeHours = 0;
         int employeeWage = 0;
-        double empPresent = Math.floor(Math.random() * 10) % 2;
+        double empPresent = Math.floor(Math.random() * 10) % 3;
         if (empPresent == FULL_TIME) {
-            System.out.println("Employee Present");
+            System.out.println("Employee Present full time");
+            employeeHours = 16;
+        } else if (empPresent==PART_TIME) {
+            System.out.println("Employee present part time");
             employeeHours = 8;
-        } else {
-            System.out.println("Employee Absent");
-            employeeHours = 0;
+        }else{
+            System.out.println("Employee absent");
+            employeeHours=0;
         }
         employeeWage = employeeHours * EMPLOYEE_WAGE_PER_HOUR;
         System.out.println("Employee Wage:" + employeeWage);
