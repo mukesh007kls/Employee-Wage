cc286c5fd45328f87c11e9e7f04b3dfe0dad195d
mukesh007kls <mallikanti2000@gmail.com>
2023-02-23 05:53:25 +0530

    ADDED:Code To Check Employee Atendence

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        int FULL_TIME = 1;
+        double empPresent = Math.floor(Math.random() * 10) % 2;
+        if (empPresent == FULL_TIME) {
+            System.out.println("Employee Present");
+        } else {
+            System.out.println("Employee Absent");
+        }
     }
 }
