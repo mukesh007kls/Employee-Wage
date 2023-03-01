ebfee1e60274fa828ec12e2fbdb6cdfd42c4de9f
mukesh007kls <mallikanti2000@gmail.com>
2023-03-01 15:56:47 +0530

    ADDED:Code for Changing to class Variable and methods

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
-    public static final int FULL_TIME = 1;
-    public static final int PART_TIME = 2;
-    public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
-    public static final int EMPLOYEE_WORKING_DAYS_PER_MONTH = 20;
-    public static final int TOTAL_WORKING_HOURS_ALLOWED = 100;
-
     public static void main(String[] args) {
+        Employee_Wage emp = new Employee_Wage();
+        emp.employee_Wage();
+    }
+}
+
+class Employee_Wage {
+    final int FULL_TIME = 1;
+    final int PART_TIME = 2;
+    final int EMPLOYEE_WAGE_PER_HOUR = 20;
+    final int EMPLOYEE_WORKING_DAYS_PER_MONTH = 20;
+    final int TOTAL_WORKING_HOURS_ALLOWED = 100;
+    void employee_Wage() {
         System.out.println("Welcome to Employee Wage Computation");
         int employeeHours = 0;
         int employeeWage = 0;
@@ -20,7 +26,7 @@ public class Main {
                     System.out.println("Employee Present full time:" + employeePresentDays + " " + employeeHours);
                     break;
                 case PART_TIME:
-                    employeePresentDays+=0.5;
+                    employeePresentDays += 0.5;
                     employeeHours += 4;
                     System.out.println("Employee present part time:" + employeePresentDays + " " + employeeHours);
                     break;
